c705cc103bb99d1945637ab39fe304035d89e814
MUHAMMAD HOZAIFA NAEEM <ssjutt2023@gmail.com>
2024-10-20 19:15:59 +0500

    Delete all files directory

--- all files/script.js
+++
@@ -1,7 +0,0 @@
-const typed = new  Typed ( '.multiple-text',{
-    strings:['Frontend Developer','Backend Developer','Ui/Ux Deveoper','Python Developer','Pentration Tester' ],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-});
